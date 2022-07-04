88e4f9fc83fe33b844df6710fd0ddc3a828fce7a
Sameer <sameerpandey388@gmail.com>
2022-07-04 23:08:49 +0530

    Added the procfile and gitignore

--- app.js
+++ app.js
@@ -48,6 +48,8 @@ app.post("/", function (req, res) {
 
 
 //Send the server on port 3000
-app.listen(3000, function () {
-    console.log("Server is running on port 3000");
-})
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
